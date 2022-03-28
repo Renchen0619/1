62eeac50f1c7d1207834a10b15e7b0c5fd8bea29
DESKTOP-FH1GRP5\dgw <renchen0619@digiwin.com>
2022-03-28 14:04:50 +0800

    1. 參數意義化 2. 一個數字只能有一位小數 3.  X 二次方 4. log、ln、√(另新增other()函式) 5. M+、M-、MC、MR功能新增 6. 除去Svg、新增按鈕

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,52 +12,100 @@ import { Location } from '@angular/common';
 
 
 export class HeroesComponent implements OnInit {
-  firstnum = '';
-  secondnum = '';
-  symbols = '';
-  display = '0';
+  firstnum = '';     // The first value in the Calculation
+  secondnum = '';    // The second value in the Calculation
+  symbols = '';      // Operator Symbols in Calculations
+  display = '0';     // Computer display value
+  memorizeNum = '0';  // Computer memorize value
+  variable = '';
+  operation = '';
 
   constructor(
     private location: Location) { }
 
   ngOnInit(): void {
   }
-
-  addNum(a: string): void {
-    if (this.display == '0'){
+  //Join the numbers
+  addNum(num: string): void {
+    if (this.display == '0'){     //  When the display is 0, the display will be empty.
       this.display = ''
+    }else if(num == '.' && this.secondnum.includes(num)){ 
+      num = '';           // When the value is a decimal, no more decimal points are allowed
     }
-    this.secondnum += String(a)  ;
-    this.display  += String(a)  ;
+    this.secondnum += String(num)  ;
+    this.display += String(num)  ;
+    // if (this.firstnum !=''){
+    //   this.display = Number(this.firstnum).toLocaleString() + this.symbols + Number(this.secondnum).toLocaleString()
+    // }else{
+    //   this.display = Number(this.secondnum).toLocaleString()
+    // }
+      
   }
-
-  addSymbol(b : string):void{
-    if(b == 'C'){
+  //Join the Symbol
+  addSymbol(sign : string):void{
+    if(sign == 'C'){
       this.firstnum = '';
       this.secondnum = '';
       this.symbols = '';
       this.display = '0';
-    }else if(b == '%'){
+      this.operation ='';
+      this.variable = '';
+    }else if(sign == '%'){
       this.display = this.firstnum + this.symbols + String(Number(this.secondnum)/100);
       this.secondnum = String(Number(this.secondnum)/100);
+    }else if(sign=='x^y'){
+      this.secondnum = String(Math.pow(Number( this.secondnum),2)) ;
+      if(this.firstnum != '' && this.symbols !=''){
+        this.doCount();
+      }else{
+        this.display = this.secondnum;
+      }
+      this.symbols = '';
+    }else if(sign == 'log' || sign == 'ln' || sign == '√￣'){
+      if (this.display == '0'){     //  When the display is 0, the display will be empty.
+        this.display = ''
+      }
+      if(this.operation != ''){ 
+        this.display = this.display.substring(0, this.display.length - (this.operation.length));
+        // When the value is a decimal, no more decimal points are allowed
+      }
+      if(this.secondnum != ''){
+        this.variable = this.secondnum;
+        this.secondnum = '';
+        this.display += sign;
+        this.operation = sign;
+      }else{
+        this.display += sign;
+        this.operation = sign;
+      }
+    }else if(this.operation != ''){
+      this.other();
+      this.doCount();
+      this.display += sign;
+      this.firstnum = this.secondnum;
+      this.secondnum = '';
+      this.symbols = sign;
     }else if(this.firstnum == '') {
       this.firstnum = this.secondnum;
       this.secondnum = '';
-      this.display += b;    
-      this.symbols = b;
+      this.display += sign;    
+      this.symbols = sign;
     }else if(this.secondnum != '' && this.symbols != ''){
       this.doCount();
-      this.display += b;
+      this.display += sign;
       this.firstnum = this.secondnum;
       this.secondnum = '';
-      this.symbols = b;
+      this.symbols = sign;
     }else{
-      this.symbols = b
-      this.display = this.firstnum + b;
+      this.symbols = sign
+      this.display = this.firstnum + sign;
     }
   }
-
+  // Count
   doCount():void{
+    if(this.operation != ''){
+      this.other();
+    }
     if(this.symbols == '+'){
       this.display = String (Number(this.firstnum) + Number(this.secondnum))
     }else if(this.symbols == '-'){
@@ -66,13 +114,21 @@ export class HeroesComponent implements OnInit {
       this.display = String (Number(this.firstnum) * Number(this.secondnum))
     }else if(this.symbols == '÷'){
       this.display = String (Number(this.firstnum) / Number(this.secondnum))
+    }else {
+      this.display = this.secondnum;
     }
     this.firstnum = '';
     this.secondnum = this.display;
   }
+  // backSpace
   backSpace():void{
     if (this.secondnum != "") {
       this.secondnum = this.secondnum.substring(0, this.secondnum.length - 1);
+    }else if(this.operation != ''){
+      this.display = this.display.substring(0, this.display.length - (this.operation.length-1));
+      this.operation = '';
+    }else if(this.variable != ''){
+      this.variable = this.variable.substring(0, this.variable.length - 1);
     }else if (this.symbols != "") {
       this.symbols = "";
       this.secondnum = this.firstnum 
@@ -85,18 +141,68 @@ export class HeroesComponent implements OnInit {
         return ;
       }
   }
+  // 
   doSign_bit() : void{
     if(this.firstnum != '' && this.secondnum != ''){
       this.doCount();
       this.display = String (Number(this.display) * (-1));
       this.secondnum = this.display;
+      this.symbols = '';
     }else if(this.secondnum != ''){
       this.display = String (Number(this.secondnum) * (-1));
       this.secondnum = this.display; 
+      this.symbols = '';
     }
-    this.symbols = '';
+    
+  }
+  Memorize(sign:string) :void{
+    if(this.secondnum != '' && this.symbols != ''){
+      this.doCount();
+    }else if(this.symbols == ''){
+      this.display = this.secondnum;
+    }else{
+      this.display = this.firstnum;
+    }
+    this.secondnum = '';
+    this.firstnum = '';
+    this.symbols = ''
+    if(sign == 'M-'){
+      this.display = String(Number(this.display) * (-1)) ;
+    }
+    this.memorizeNum = String(Number(this.memorizeNum)  + Number(this.display));
+    this.display = '0'
+    if(sign =='MR'){
+      this.display = this.memorizeNum;
+      this.secondnum = this.memorizeNum
+      this.memorizeNum = '0'
+    }else if(sign =='MC'){
+      this.memorizeNum = '0';
+    }
   }
   goBack():void{
     this.location.back();
+  }
+  other():void{
+    if(this.operation == 'ln'){
+      if(this.variable != ''){
+        this.secondnum = String(Number(this.variable) * Math.log(Number(this.secondnum)));
+      }else{
+        this.secondnum =  String(Math.log(Number(this.secondnum)));
+      }
+    }else if(this.operation == 'log'){
+      if(this.variable != ''){
+        this.secondnum = String(Number(this.variable) * Math.log10(Number(this.secondnum)));
+      }else{
+        this.secondnum =  String(Math.log10(Number(this.secondnum)));
+      }
+    }else if(this.operation == '√￣'){
+      if(this.variable != ''){
+        this.secondnum = String(Number(this.variable) * Math.sqrt(Number(this.secondnum)));
+      }else{
+        this.secondnum =  String(Math.sqrt(Number(this.secondnum)));
+      }
+    }
+    this.operation = '';
+    this.variable ='';
   }
 }
